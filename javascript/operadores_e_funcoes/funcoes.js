826c1aa09d545c674e41f848a4aef6161d877156
Carlos-Eduardo-Costa <carlos.e.costa53@aluno.senai.br>
2026-02-03 15:42:41 -0300

    03/02/2026

---
+++ javascript/operadores_e_funcoes/funcoes.js
@@ -0,0 +1,15 @@
+function nomeDaFuncao(parametros) {
+    console.log("ola, javascript");
+}
+
+saudacao();
+
+function somar(a, b) {
+    return a + b;
+}
+console.log("Soma: ", somar(5, 3));
+
+function saudacaoNome(nome) {
+    console.log("Olá, " + nome);
+} 
+saudacaoNome("Carlos");
--- javascript/operadores_e_funcoes/script.js
+++ javascript/operadores_e_funcoes/script.js
@@ -2,11 +2,11 @@ console.log("ola, javascript");
 
 let a=10;
 let b=3;
-console.log(a+b);
-console.log(a-b);
-console.log(a*b);
-console.log(a/b);
-console.log(a%b);
+console.log( "adição: " , a+b);
+console.log(" subtração: " , a-b);
+console.log( "multiplicação: " , a*b);
+console.log(" divisão: " , a/b);
+console.log(" resto da divisão: " , a%b);
 
 let contador=5;
 contador++;
@@ -26,3 +26,46 @@ console.log(x == y);
 console.log(x === y);
 console.log(x != y);
 console.log(x !== y);
+
+let idade=20;
+if (idade >= 18) {
+    console.log("Maior de idade");
+} else {
+    console.log("Menor de idade");
+}   
+let numero1=4;
+let numero2=8;
+if (numero1 > numero2) {
+    console.log("Número 1 é maior");
+} else if (numero1 < numero2) {
+    console.log("Número 2 é maior");
+}
+else {
+    console.log("Números são iguais");
+}
+
+let idade2 = 20;
+let temCarteiraDeMotorista = true;
+
+console.log(idade >= 18 && temCarteiraDeMotorista);
+
+let chovendo = false;
+let guardaChuva = true;
+
+console.log(chovendo || guardaChuva);
+
+let ligado = false;
+console.log(!ligado);//true
+
+let frequencia= 73;
+let nota= 8;
+console.log(frequencia >= 75 && nota >= 7);
+
+let senha= "senha123";
+let token= true;
+
+if (senha === "senha123" || token === true) {
+    console.log("Acesso concedido");
+} else {
+    console.log("Acesso negado");
+}
